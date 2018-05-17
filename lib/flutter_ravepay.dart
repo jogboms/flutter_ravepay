b41a4271e7da95c6a69eca6cc2cb52a888cf4ef4
Jogboms <jeremiahogbomo@gmail.com>
2018-05-17 10:10:57 +0100

    chore: platform-aware channels

--- lib/flutter_ravepay.dart
+++ lib/flutter_ravepay.dart
@@ -6,6 +6,7 @@ class FlutterRavepayResult {
   final Map result;
   FlutterRavepayResult(this.result);
 
+  get status => result['status'];
   get message => result['message'];
 
   toMap() {
@@ -14,9 +15,11 @@ class FlutterRavepayResult {
 }
 
 class FlutterRavepay {
-  static const MethodChannel _channel = const MethodChannel('ng.i.handikraft/flutter_ravepay');
+  static const MethodChannel _androidChannel = const MethodChannel('ng.i.handikraft/flutter_ravepay');
+  static const MethodChannel _iosChannel = const MethodChannel('ng.i.handikraft/flutter_ravepay_local');
 
   static Future<FlutterRavepayResult> chargeCard(Map<String, dynamic> chargeOptions) async {
+    MethodChannel _channel = TargetPlatform.iOS ? _iosChannel : _androidChannel;
     Map result = await _channel.invokeMethod('chargeCard', chargeOptions);
     return new FlutterRavepayResult(result);
   }
