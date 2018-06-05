48b72f8144f52385151d04d60b7607b83a6d6a2a
Jogboms <jeremiahogbomo@gmail.com>
2018-06-05 17:20:00 +0100

    refactor: RavepayCard

--- lib/flutter_ravepay.dart
+++ lib/flutter_ravepay.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:flutter/foundation.dart';
 
@@ -59,79 +60,116 @@ class RavepayConfig {
   }
 }
 
+List<Map<dynamic, dynamic>> _parseTokenList(List<dynamic> tokens) {
+  if (tokens == null) return [];
+
+  return new List.from(tokens.map((token) => Map.from(token)));
+}
+
+class RavepayCard {
+  String expirymonth;
+  String expiryyear;
+  String cardBIN;
+  String last4digits;
+  String brand;
+  List<Map<dynamic, dynamic>> card_tokens;
+  String life_time_token;
+
+  RavepayCard({
+    this.expirymonth,
+    this.expiryyear,
+    this.cardBIN,
+    this.last4digits,
+    this.brand,
+    this.card_tokens,
+    this.life_time_token,
+  });
+
+  factory RavepayCard.fromMap(Map map) {
+    return new RavepayCard(
+      expirymonth: map["expirymonth"],
+      expiryyear: map["expiryyear"],
+      cardBIN: map["cardBIN"],
+      last4digits: map["last4digits"],
+      brand: map["brand"],
+      card_tokens: _parseTokenList(map["card_tokens"]),
+      life_time_token: map["life_time_token"],
+    );
+  }
+}
+
 class RavepayResult {
   int id;
+  int merchant_id;
+  int customer_id;
+  int is_live;
   double amount;
+  double appfee;
+  double merchantfee;
   double charged_amount;
-  String authurl;
-  String chargeResponseMessage;
-  int merchantbearsfee;
-  int customerId;
-  int merchantfee;
-  String modalauditid;
-  String currency;
-  String narration;
-  DateTime updatedAt;
-  String paymentPlan;
-  String paymentId;
-  bool customercandosubsequentnoauth;
+  double merchantbearsfee;
+  String tx_ref;
+  String flw_ref;
+  String order_ref;
+  String rave_ref;
+  String transaction_currency;
+  String payment_entity;
+  String payment_id;
   String charge_type;
-  String paymentType;
-  String orderRef;
-  String txRef;
-  String flwRef;
-  String raveRef;
-  String user_token;
-  String embed_token;
+  String fraud_status;
+  String narration;
+  DateTime createdAt;
+  DateTime updatedAt;
+  RavepayCard card;
 
+  String __status__;
   String __status;
   String __message;
 
-  final Map __result;
+  final Map __payload;
 
-  RavepayResult(this.__result) {
-    if (!isCancel && __result['payload'] != null) {
-      __status = __result['payload']['status'];
-      __message = __result['payload']['message'];
-      final Map data = __result['payload']['data'];
+  RavepayResult(this.__status__, this.__payload) {
+    if (!isCancel && __payload != null) {
+      __status = __payload['status'];
+      __message = __payload['message'];
+      final Map data = __payload['data'];
 
       if (data != null) {
         id = int.tryParse(data["id"].toString());
-        paymentPlan = data["paymentPlan"];
-        updatedAt = DateTime.tryParse(data["updatedAt"]);
-        narration = data["narration"];
-        currency = data["currency"];
-        modalauditid = data["modalauditid"];
         amount = double.tryParse(data["amount"].toString());
+        order_ref = data["order_ref"];
+        rave_ref = data["rave_ref"];
+        is_live = int.tryParse(data["id"].toString());
+        appfee = double.tryParse(data["appfee"].toString());
+        merchantfee = double.tryParse(data["merchantfee"].toString());
         charged_amount = double.tryParse(data["charged_amount"].toString());
-        merchantfee = int.tryParse(data["merchantfee"].toString());
-        customerId = int.tryParse(data["customerId"].toString());
-        merchantbearsfee = int.tryParse(data["merchantbearsfee"].toString());
-        chargeResponseMessage = data["chargeResponseMessage"];
-        authurl = data["authurl"];
-        paymentId = data["paymentId"];
-        customercandosubsequentnoauth = data["customercandosubsequentnoauth"].toString() == "true";
+        merchantbearsfee = double.tryParse(data["merchant_id"].toString());
+        merchant_id = int.tryParse(data["merchantbearsfee"].toString());
+        customer_id = int.tryParse(data["customer.id"].toString());
+        transaction_currency = data["transaction_currency"];
+        payment_entity = data["payment_entity"];
+        payment_id = data["payment_id"];
         charge_type = data["charge_type"];
-        paymentType = data["paymentType"];
-        orderRef = data["orderRef"];
-        txRef = data["txRef"];
-        flwRef = data["flwRef"];
-        raveRef = data["raveRef"];
-        user_token = data["chargeToken"]["user_token"];
-        embed_token = data["chargeToken"]["embed_token"];
+        fraud_status = data["fraud_status"];
+        narration = data["narration"];
+        createdAt = DateTime.tryParse(data["createdAt"]);
+        updatedAt = DateTime.tryParse(data["updatedAt"]);
+        tx_ref = data["tx_ref"];
+        flw_ref = data["flw_ref"];
+        card = data["card"] != null ? RavepayCard.fromMap(data["card"]) : null;
       }
     }
   }
 
-  get isSuccess => __result['status'] == "SUCCESS";
-  get isError => __result['status'] == "ERROR";
-  get isCancel => __result['status'] == "CANCELLED";
+  get isSuccess => __status__ == "SUCCESS";
+  get isError => __status__ == "ERROR";
+  get isCancel => __status__ == "CANCELLED";
 
   getStatus() => __status;
 
   getMessage() => __message;
 
-  toMap() => !isCancel ? __result['payload']['data'] : null;
+  toMap() => !isCancel ? __payload['payload']['data'] : null;
 }
 
 class Ravepay {
@@ -142,7 +180,8 @@ class Ravepay {
   Ravepay(this.context);
 
   Future<RavepayResult> chargeCard(RavepayConfig config) async {
-    String _channelName = Theme.of(context).platform == TargetPlatform.iOS ? _iosChannel : _androidChannel;
+    bool isIos = Theme.of(context).platform == TargetPlatform.iOS;
+    String _channelName = isIos ? _iosChannel : _androidChannel;
     MethodChannel _channel = new MethodChannel(_channelName);
     Map result;
     try {
@@ -151,7 +190,10 @@ class Ravepay {
     } on PlatformException {
       result = {"status": "CANCELLED"};
     }
-    return new RavepayResult(result);
+    return new RavepayResult(
+      result["status"],
+      result["payload"] != null ? (isIos ? result["payload"] : json.decode(result["payload"])) : null,
+    );
   }
 
   static of(BuildContext context) {
